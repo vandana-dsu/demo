4a41e7d6a46eb62e0cdac0676260d7cbce5438bc
vandana-dsu <141351192+vandana-dsu@users.noreply.github.com>
2023-11-16 21:45:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,68 @@
+# EC2 instance variables
+variable "ami" {
+  description = "Amazon Machine Image (AMI) ID for the EC2 instance"
+  default     = "your-ami-id"  # Replace with your desired AMI ID
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  default     = "your-instance-type"  # Replace with your desired instance type
+}
+
+variable "git_repo" {
+  description = "Git repository URL containing Dockerfiles and docker-compose.yml"
+  default     = "https://github.com/yourusername/your-docker-repo.git"  # Replace with your Git repository URL
+}
+
+# EC2 instance resource
+resource "aws_instance" "example_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  # Other instance configuration options can be added here
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y git docker
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              git clone ${var.git_repo} /tmp/repo
+              cd /tmp/repo
+              docker-compose up -d
+              EOF
+
+  # Security group for allowing HTTP, SSH, and Docker traffic
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+}
+
+# Security group resource
+resource "aws_security_group" "instance_sg" {
+  name        = "instance_sg"
+  description = "Security group for the EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
